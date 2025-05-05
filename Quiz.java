29e79b5fc986f6833783b0c4319a01b43637f130
mary kenny <marykenny2704@yahoo.com>
2025-05-05 14:35:58 +0200

    docs: add ejercicio Quiz

--- Quiz.java
+++ Quiz.java
@@ -1,42 +1,75 @@
+import java.util.Scanner;
+
 public class Quiz {
     public static void main(String[] args) {
         //Escribe un programa que sea un pequeño juego trivial de acertar preguntas
         //Cada pregunta tendrá 4 opciones que el usuario podrá escoger
         //Por cada respuesta correcta, añade 5 puntos al puntaje
+
+        //Las opciones a, b, c y d de cada pregunta, deberán dejar en terminal un espacio de sangría, averigua como hacerlo en java
+
+        int points = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("P fv teclea la(s) letra(s) de tu respuesta. ");
+        //Preguntas:
+        System.out.println("1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?");
+        System.out.println("\t a. Ada Lovelace");
+        System.out.println("\t b. Jude Milhon");
+        System.out.println("\t c. Martha Ackelsberg");
+        System.out.println("\t d. Jane Jacobs");
+        String answer = scanner.nextLine();
+        if (answer.equals("b")) {
+            points = points + 5;
+        }
+
+        System.out.println("2. ¿Quién es Fuencisla Clemares?");
+        System.out.println("\t a. La directora general de IBM Europa, Oriente Medio y África");
+        System.out.println("\t b. La vicepresidenta de Microsoft Western Europe");
+        System.out.println("\t c. La directora general de Google en España y Portugal");
+        System.out.println("\t d. Fundadora y CEO de Marsi Bionics");
+        answer = scanner.nextLine();
+        if (answer.equals("c")) {
+            points = points + 5;
+        }
+
+
+        System.out.println("3. ¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr?");
+        System.out.println("\t a. Primera programadora de software");
+        System.out.println("\t b. Una actriz de cine");
+        System.out.println("\t c. Inventora del procesador de datos");
+        System.out.println("\t d. Inventora del sistema de comunicaciones de salto de frecuencia, base del wifi, bluetooth y GPS");
+        answer = scanner.nextLine();
+        if ((answer.equals("bd")) || (answer.equals("db"))) {
+            points = points + 5;
+        }
+
+
+        System.out.println("4. ¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?");
+        System.out.println("\t a. Katherine Johnson, Dorothy Vaughan y Mary Jackson");
+        System.out.println("\t b. Jessica Miller, Emily Johnson, Sarah Davis");
+        System.out.println("\t c. bell hooks, Toni Morrison y Alice Walker");
+        System.out.println("\t d. Emmeline Pankhurst, Millicent Fawcett y Harriet Harman");
+        answer = scanner.nextLine();
+        if (answer.equals("a")) {
+            points = points + 5;
+        }
+        System.out.println("Has ganado " + points + " puntos /20.");
         //Al finalizar el juego imprime cual fue tu puntaje /20
         //Si el puntaje es 15 o más, imprime: Tú si que sabes mucho
         //Si el puntaje es menor de 15 pero mayor o igual a 5, imprime: No lo has hecho mal
         //De lo contrario imprime: Buena suerte la próxima vez
-        //Las opciones a, b, c y d de cada pregunta, deberán dejar en terminal un espacio de sangría, averigua como hacerlo en java
+        if (points >= 15) {
+            System.out.println("Tú si que sabes mucho");
+        } else if (points >= 5) {
+            System.out.println("No lo has hecho mal");
+        } else {
+            System.out.println("Buena suerte la próxima vez");
+        }
 
-        //Preguntas:
-        /* 1. ¿Quién fue la hacker que escribió en 1994 Hacking the Wetware?
-                a. Ada Lovelace
-                b. Jude Milhon
-                c. Martha Ackelsberg
-                d. Jane Jacobs
-            
-            2. ¿Quién es Fuencisla Clemares?
-                a. La directora general de IBM Europa, Oriente Medio y África
-                b. La vicepresidenta de Microsoft Western Europe
-                c. La directora general de Google en España y Portugal
-                d. Fundadora y CEO de Marsi Bionics
-            
-            3. ¿Quién fue Hedwig Eva Maria Kiesler, conocida como Hedy Lamarr?
-                a. Primera programadora de software
-                b. Una actriz de cine
-                c. Inventora del procesador de datos
-                d. Inventora del sistema de comunicaciones de salto de frecuencia, base del wifi, bluetooth y GPS
-            
-            4. ¿Cómo se llamaban las principales 'mujeres ocultas' de la NASA, encargadas de los cálculos para que la misión del Apolo 11 concluyera con éxito?
-                a. Katherine Johnson, Dorothy Vaughan y Mary Jackson
-                b. Jessica Miller, Emily Johnson, Sarah Davis
-                c. bell hooks, Toni Morrison y Alice Walker
-                d. Emmeline Pankhurst, Millicent Fawcett y Harriet Harman
-        */
 
         //Respuestas: 1. b / 2. c / 3. b y d / 4. a
 
+        scanner.close();
     }
-    
+
 }
